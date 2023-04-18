3cc815ab6e241ae0029624e9009ee128687cf1d2
dandevweb <dandevweb@gmail.com>
2023-04-18 07:23:03 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,37 +1,24 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
+import { routes } from './routes.js';
 
-const database = new Database;
+
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  console.log(req.body)
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-
-    return res.end(JSON.stringify(users));
+  if (!route) {
+    return res.writeHead(404).end()
   }
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+  route.handler(req, res)
 
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
-  res.writeHead(404).end();
 })
 
 server.listen(3333)
