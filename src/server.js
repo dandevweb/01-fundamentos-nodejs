8ac30c6be97e2ddb439750ed514b56f336d749fe
dandevweb <dandevweb@gmail.com>
2023-04-17 21:14:48 -0300

    http methods

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagen de usuários');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
   res.end('Hello World');
 })
 
