043b23f030caf810e00fefcbe5a9dff3700059f3
dandevweb <dandevweb@gmail.com>
2023-04-17 22:01:11 -0300

    stream write and transform

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,7 +1,7 @@
 // process.stdin
 //   .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform, Duplex } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -20,4 +20,22 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
