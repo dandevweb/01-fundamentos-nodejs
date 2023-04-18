5663bc81c06101dc5b33885cf79e0a57878161ba
dandevweb <dandevweb@gmail.com>
2023-04-17 21:24:41 -0300

    save data in memory

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http';
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagen de usuários');
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      'id': users.length + 1,
+      'name': 'John Doe',
+      'email': 'j@j.com'
+    })
+
     return res.end('Criação de usuário')
   }
   res.end('Hello World');
